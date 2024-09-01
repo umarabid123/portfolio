25bde8c61f15f7c96f44e95ee356fcd247f1694e
umarabid123 <umarabid709@gmail.com>
2024-09-01 23:38:45 +0500

    deploy

--- src/Components/projectCard/Project_Card.jsx
+++ src/Components/projectCard/Project_Card.jsx
@@ -10,7 +10,7 @@ const Project_Card = (props) => {
   
   return (
     <div className=" bg-[#172a57] p-8 w-[90%] sm:w-[46%] mx-auto lg:w-[30%] flex flex-col gap-4 rounded-lg my-4" data-aos ="fade-up">
-      <img src={props.img} className="w-96 h-60 rounded-lg " alt="" />
+      <img src={props.img} className="w-96 h-44 rounded-lg " alt="" />
       <p className="text-purple-500 font-semibold text-xs py-3">
         <span className="bg-gray-600 p-2 rounded-lg">{props.tecnholgyF}</span>
         <span className="bg-gray-600 p-2 rounded-lg m-2">
--- src/data.jsx
+++ src/data.jsx
@@ -1,5 +1,14 @@
 
 import hanger from './assets/Images/hanger.png'
+import amazon from './assets/Images/Amazon.png'
+import animated from './assets/Images/animate.png'
+import portfolio from './assets/Images/portfolio.png'
+import discord from './assets/Images/discord.png'
+import thread from './assets/Images/thread.png'
+import olx from './assets/Images/olx.png'
+import netflix from './assets/Images/netflix.png'
+import ary from './assets/Images/ary.png'
+
 export const data = [
     {
       img: hanger,
@@ -11,48 +20,76 @@ export const data = [
         "A Rule Builder application “Decision Hub” that empowers Business Analysts to create, save, and",
     },
     {
-      img: hanger,
+      img: amazon,
       tecnholgyF: "React JS",
       tecnholgy: "Tailwind CSS",
-      name: "Hanger Direct",
+      name: "Amazon Clone",
       date: "Jan 2024 - Dec 2023",
       paragraph:
         "A Rule Builder application “Decision Hub” that empowers Business Analysts to create, save, and",
     },
     {
-      img: hanger,
+      img: animated,
       tecnholgyF: "React JS",
       tecnholgy: "Tailwind CSS",
-      name: "Hanger Direct",
+      name: "Animated Porfolio",
       date: "Jan 2024 - Dec 2023",
       paragraph:
         "A Rule Builder application “Decision Hub” that empowers Business Analysts to create, save, and",
     },
     {
-      img: hanger,
+      img: portfolio,
       tecnholgyF: "React JS",
       tecnholgy: "Tailwind CSS",
-      name: "Hanger Direct",
+      name: "Porfolio",
       date: "Jan 2024 - Dec 2023",
       paragraph:
         "A Rule Builder application “Decision Hub” that empowers Business Analysts to create, save, and",
     },
     {
-      img: hanger,
+      img: discord,
       tecnholgyF: "React JS",
       tecnholgy: "Tailwind CSS",
-      name: "Hanger Direct",
+      name: "Discord Clone",
       date: "Jan 2024 - Dec 2023",
       paragraph:
         "A Rule Builder application “Decision Hub” that empowers Business Analysts to create, save, and",
     },
     {
-      img: hanger,
+      img: thread,
       tecnholgyF: "React JS",
       tecnholgy: "Tailwind CSS",
-      name: "Hanger Direct",
+      name: "Thread Net",
       date: "Jan 2024 - Dec 2023",
       paragraph:
         "A Rule Builder application “Decision Hub” that empowers Business Analysts to create, save, and",
     },
+    {
+      img: netflix,
+      tecnholgyF: "React JS",
+      tecnholgy: "Tailwind CSS",
+      name: "Netflix Clone",
+      date: "Jan 2024 - Dec 2023",
+      paragraph:
+        "A Rule Builder application “Decision Hub” that empowers Business Analysts to create, save, and",
+    },
+    {
+      img: olx,
+      tecnholgyF: "React JS",
+      tecnholgy: "Tailwind CSS",
+      name: "Olx Clone",
+      date: "Jan 2024 - Dec 2023",
+      paragraph:
+        "A Rule Builder application “Decision Hub” that empowers Business Analysts to create, save, and",
+    },
+    {
+      img: ary,
+      tecnholgyF: "React JS",
+      tecnholgy: "Tailwind CSS",
+      name: "Ary Zap Clone",
+      date: "Jan 2024 - Dec 2023",
+      paragraph:
+        "A Rule Builder application “Decision Hub” that empowers Business Analysts to create, save, and",
+    },
+    
   ];
