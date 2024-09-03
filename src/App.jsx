df81f71e61abe72ed9958e570fdcbae07b68fbe5
umarabid123 <umarabid709@gmail.com>
2024-09-03 19:25:52 +0500

    deploy

--- src/App.jsx
+++ src/App.jsx
@@ -62,15 +62,15 @@ function App() {
     loop: {},
   });
 
-useEffect(() =>{
-  Aos.init({duration: 2000})
-}, [])
+  useEffect(() => {
+    Aos.init({ duration: 2000 })
+  }, [])
 
   return (
     <div className="bg-[#0a192f] h-full lg:px-6 xl:px-0 overflow-hidden" >
 
 
-<div className="w-full absolute inset-0 h-full">
+      <div className="w-full absolute inset-0 h-full">
         <SparklesCore
           id="tsparticlesfullpage"
           background="transparent"
@@ -83,7 +83,7 @@ useEffect(() =>{
       </div>
       <div className="container xl:max-w-6xl mx-auto relative z-20">
         {" "}
-        <Navbar data-aos ='fade-up' />
+        <Navbar data-aos='fade-up' />
         {/* Hero Section */}
         <div className="main-hero lg:mt-14 flex flex-col-reverse lg:flex-row justify-between items-center px-6 z-20">
           {" "}
@@ -105,26 +105,27 @@ useEffect(() =>{
               is both functional and aesthetically pleasing.
             </p>
 
+            icons
             <div className="icons flex justify-center sm:justify-start items-center gap-4 mt-6">
               <a href="https://www.facebook.com/profile.php?id=100043144280043">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 512 512"
-                fill="#64ffda"
-                className="w-7"
-              >
-                <path d="M512 256C512 114.6 397.4 0 256 0S0 114.6 0 256C0 376 82.7 476.8 194.2 504.5V334.2H141.4V256h52.8V222.3c0-87.1 39.4-127.5 125-127.5c16.2 0 44.2 3.2 55.7 6.4V172c-6-.6-16.5-1-29.6-1c-42 0-58.2 15.9-58.2 57.2V256h83.6l-14.4 78.2H287V510.1C413.8 494.8 512 386.9 512 256h0z" />
-              </svg>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 512 512"
+                  fill="#64ffda"
+                  className="w-7"
+                >
+                  <path d="M512 256C512 114.6 397.4 0 256 0S0 114.6 0 256C0 376 82.7 476.8 194.2 504.5V334.2H141.4V256h52.8V222.3c0-87.1 39.4-127.5 125-127.5c16.2 0 44.2 3.2 55.7 6.4V172c-6-.6-16.5-1-29.6-1c-42 0-58.2 15.9-58.2 57.2V256h83.6l-14.4 78.2H287V510.1C413.8 494.8 512 386.9 512 256h0z" />
+                </svg>
               </a>
               <a href="www.linkedin.com/in/muhammad-umar-abid-webdev">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 448 512"
-                className="w-6"
-                fill="#64ffda"
-              >
-                <path d="M416 32H31.9C14.3 32 0 46.5 0 64.3v383.4C0 465.5 14.3 480 31.9 480H416c17.6 0 32-14.5 32-32.3V64.3c0-17.8-14.4-32.3-32-32.3zM135.4 416H69V202.2h66.5V416zm-33.2-243c-21.3 0-38.5-17.3-38.5-38.5S80.9 96 102.2 96c21.2 0 38.5 17.3 38.5 38.5 0 21.3-17.2 38.5-38.5 38.5zm282.1 243h-66.4V312c0-24.8-.5-56.7-34.5-56.7-34.6 0-39.9 27-39.9 54.9V416h-66.4V202.2h63.7v29.2h.9c8.9-16.8 30.6-34.5 62.9-34.5 67.2 0 79.7 44.3 79.7 101.9V416z" />
-              </svg>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 448 512"
+                  className="w-6"
+                  fill="#64ffda"
+                >
+                  <path d="M416 32H31.9C14.3 32 0 46.5 0 64.3v383.4C0 465.5 14.3 480 31.9 480H416c17.6 0 32-14.5 32-32.3V64.3c0-17.8-14.4-32.3-32-32.3zM135.4 416H69V202.2h66.5V416zm-33.2-243c-21.3 0-38.5-17.3-38.5-38.5S80.9 96 102.2 96c21.2 0 38.5 17.3 38.5 38.5 0 21.3-17.2 38.5-38.5 38.5zm282.1 243h-66.4V312c0-24.8-.5-56.7-34.5-56.7-34.6 0-39.9 27-39.9 54.9V416h-66.4V202.2h63.7v29.2h.9c8.9-16.8 30.6-34.5 62.9-34.5 67.2 0 79.7 44.3 79.7 101.9V416z" />
+                </svg>
               </a>
               <svg
                 xmlns="http://www.w3.org/2000/svg"
@@ -156,7 +157,7 @@ useEffect(() =>{
         </div>
 
         {/* Skills Section  */}
-        <div className="skills pb-10 pt-36 px-6 " data-aos ="fade-up">
+        <div className="skills pb-10 pt-36 px-6 " data-aos="fade-up">
           <h1 className="text-white text-center text-5xl font-semibold" >
             Skills
           </h1>
@@ -172,10 +173,10 @@ useEffect(() =>{
 
         {/* Project  Section  */}
         <div className="project pt-36">
-          <h1 className="text-white text-center text-5xl font-semibold" data-aos ="fade-left">
+          <h1 className="text-white text-center text-5xl font-semibold" data-aos="fade-left">
             Project
           </h1>
-          <p className="text-gray-300 text-xl text-center py-10" data-aos ="fade-left">
+          <p className="text-gray-300 text-xl text-center py-10" data-aos="fade-left">
             Here are some of my skills on which I have been working
           </p>
           <div className="flex justify-between flex-wrap">
@@ -189,25 +190,25 @@ useEffect(() =>{
                   name={item.name}
                   date={item.date}
                   paragraph={item.paragraph}
-                  
+
                 />
               );
             })}
           </div>
         </div>
 
-         {/* About Section  */}
+        {/* About Section  */}
         <div className="about my-16 w-[90%] mx-auto">
           <About />
         </div>
-         
-      {/* Contact Section  */}
+
+        {/* Contact Section  */}
         <div className="Contact">
           <Contact />
         </div>
         <div className="last-text py-20">
           <p className="text-[#64ffda] text-xl text-center font-medium">Muhammad Umar</p>
-          <p className="text-[#fff] text-center font-medium">© 2024 Muhammad Umar. All rights reserved.</p> 
+          <p className="text-[#fff] text-center font-medium">© 2024 Muhammad Umar. All rights reserved.</p>
         </div>
       </div>
     </div>
